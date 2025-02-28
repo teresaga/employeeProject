b6a14fbeb62b44bda4b3e36a1dd6ba13a87dc926
Teresa <teresagalaviza@gmail.com>
2025-02-28 15:19:05 -0700

    Security config: user and roles added.

---
+++ src/main/java/org/example/emproyeeproject/security/DemoSecurityConfig.java
@@ -0,0 +1,61 @@
+package org.example.emproyeeproject.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+
+    // This will ignore the user of application.properties file
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails teresa = User.builder()
+                .username("Teresa")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        UserDetails jose = User.builder()
+                .username("Jose")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails mario = User.builder()
+                .username("Mario")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        return new InMemoryUserDetailsManager(teresa, jose, mario);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "api/employees/**").hasRole("ADMIN")
+        );
+
+        // use HTTP Basic Authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request Forgery (CSRF)
+        // In general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
