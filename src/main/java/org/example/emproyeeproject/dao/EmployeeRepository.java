364adce113bcdc6b150f78a5594e106527036417
Teresa <teresagalaviza@gmail.com>
2025-02-28 14:24:22 -0700

    Add Spring Data REST properties

--- src/main/java/org/example/emproyeeproject/dao/EmployeeRepository.java
+++ src/main/java/org/example/emproyeeproject/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package org.example.emproyeeproject.dao;
 
 import org.example.emproyeeproject.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
