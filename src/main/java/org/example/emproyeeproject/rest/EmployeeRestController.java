544a16d3cf206d95e5b63d34b712187ce899bd7e
Teresa <teresagalaviza@gmail.com>
2025-02-27 18:37:57 -0700

    Employee endpoints added

--- src/main/java/org/example/emproyeeproject/rest/EmployeeRestController.java
+++ src/main/java/org/example/emproyeeproject/rest/EmployeeRestController.java
@@ -4,10 +4,7 @@ import org.example.emproyeeproject.dao.EmployeeDAOJpaImpl;
 import org.example.emproyeeproject.entity.Employee;
 import org.example.emproyeeproject.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +26,46 @@ public class EmployeeRestController {
 
     @GetMapping("/employees/{employeeId}")
     public Employee findById(@PathVariable int employeeId) {
-        return employeeService.findById(employeeId);
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        // also just in case if they pass an id in JSON ....  set id to 0
+        // this is to force a save of a new item, instead of update
+        employee.setId(0);
+
+        Employee savedEmployee = employeeService.save(employee);
+
+        return savedEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        Employee savedEmployee = employeeService.save(employee);
+
+        return savedEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+
+        // check if employee exists
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        // delete employee
+        employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
     }
 }
